66bd18680163c09d2b6d9c2c6f0b19866f68a59c
Luis Mendoza <luis.mendoza.alonso@hotmail.com>
2023-03-30 20:29:05 -0500

    Creacion de repositorio

---
+++ src/main/java/com/platzi/maker/PlatziMakerApplication.java
@@ -0,0 +1,13 @@
+package com.platzi.maker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatziMakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatziMakerApplication.class, args);
+	}
+
+}
